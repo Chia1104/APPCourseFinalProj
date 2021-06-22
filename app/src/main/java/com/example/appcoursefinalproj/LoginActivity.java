e420aba986c741be74cc6bc544a8f99fa15b4c8c
hannkkt <38397958+hannkkt@users.noreply.github.com>
2021-06-23 00:17:29 +0800

    1.999

--- app/src/main/java/com/example/appcoursefinalproj/LoginActivity.java
+++ app/src/main/java/com/example/appcoursefinalproj/LoginActivity.java
@@ -6,21 +6,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import static android.content.ContentValues.TAG;
 
 public class LoginActivity extends AppCompatActivity {
 
     TextView tv1, tv2, tv3;
     TextInputEditText etRegEmail, etRegPassword;
-    Button btnLogin;
+    Button btnLogin, GooglesignInButton;
 
     FirebaseAuth mAuth;
 
@@ -43,6 +56,14 @@ public class LoginActivity extends AppCompatActivity {
         tv3.setOnClickListener(view ->{
             startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
         });
+//
+//        GooglesignInButton = (Button) findViewById(R.id.google_sign_in_button);
+//        GooglesignInButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(LoginActivity.this, GoogleSignInActivity.class));
+//            }
+//        });
     }
 
     private void loginUser() {
